4ac37c086d4ab7278fbcec6f23e8f6bdfd5df23d
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2021-06-11 11:20:03 -0700

    Update 000backup.sh

--- 000backup.sh
+++ 000backup.sh
@@ -1,1 +1,74 @@
 
+printf "H"
+clear
+sleep 0.1
+printf "Hi"
+sleep 0.1
+clear
+printf "Hi \n W"
+sleep 0.1
+clear
+printf "Hi \n We"
+sleep 0.1
+clear
+printf "Hi \n We "
+sleep 0.1
+clear
+printf "Hi \n We H"
+sleep 0.1
+clear
+printf "Hi \n We Ha"
+sleep 0.1
+clear
+printf "Hi \n We Hav"
+sleep 0.1
+clear
+printf "Hi \n We Have"
+sleep 0.1
+clear
+printf "Hi \n We Have "
+sleep 0.1
+clear
+printf "Hi \n We Have A"
+sleep 0.1
+clear
+printf "Hi \n We Have Ac"
+sleep 0.1
+clear
+printf "Hi \n We Have Acc"
+sleep 0.1
+clear
+printf "Hi \n We Have Acce"
+sleep 0.1
+clear
+printf "Hi \n We Have Acces"
+sleep 0.1
+clear
+print "Hi \n We Have Access"
+sleep 0.1
+clear
+printf "Hi \n We Have Access "
+sleep 0.1
+clear
+printf "Hi \n We Have Access T"
+sleep 0.1
+clear
+printf "Hi \n We Have Access To"
+sleep 0.1
+clear
+printf "Hi \n We Have Access To "
+sleep 0.1
+clear
+printf "Hi \n We Have Access To Y"
+sleep 0.1
+clear
+printf "Hi \n We Have Access To Yo"
+sleep 0.1
+clear
+printf "Hi \n We Have Access To You"
+sleep 0.1
+clear
+printf "Hi \n We Have Access To Your"
+sleep 0.1
+clear
+read -p "Enter Your Name: "  username; echo "Hi $username"; if [ $username == "Bobcat" ]; exit; fi
