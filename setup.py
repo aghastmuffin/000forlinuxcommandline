a183fd80a915868a978556a822922c7857d3aeb3
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2021-06-11 11:25:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 import os
+var = int(50)
 try:
-  os.system("sudo chmod a+x 000")
-  os.system("echo Setup complete!")
+    while var > 0:
+        os.system('cd')
+        var = (var - 1)
+    os.system("cd")
+    os.system("sudo chmod a+x 000")
+    os.system("echo Setup complete!")
 except:
-  os.system("echo Uh OH there was an error! (make sure you have access to sudo)")
-  
+    os.system("echo Uh OH there was an error! (make sure you have access to sudo)")
